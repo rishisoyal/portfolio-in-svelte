14dffdc1eeff7fd9d7a613873bba300d2d762929
rishisoyal <rishisoyal510@gmail.com>
2026-03-24 15:29:09 +0530

    reduce image size to improve performance

--- src/routes/portfolio/data/projects.ts
+++ src/routes/portfolio/data/projects.ts
@@ -4,7 +4,7 @@ export const projects: Array<Project> = [
   {
     name: "Code Reviewer(AI Agent)",
     desc: "Implemented an autonomous self-correcting agent using LLM tool-calling and Zod-validated feedback units. The agent reviews and improves code snippets based on user requirements, leveraging Mistral AI models and Vercel's AI SDK for seamless integration.",
-    img: "/code-reviewer-browser-mockup.jpeg",
+    img: "/code-reviewer-browser-mockup.avif",
     public_url: "https://code-reviewer-theta-one.vercel.app/",
     github: "https://github.com/rishisoyal/Code-Reviewer",
     techStack: ["NextJS", "Vercel AI SDK", "Zod", "Typescript", "Mistral AI"],
@@ -12,7 +12,7 @@ export const projects: Array<Project> = [
   {
     name: "Weather App",
     desc: "A sleek, modern web application that displays current weather and 3-day forecast for any city using the WeatherAPI API. Users can also get local weather data automatically via IP address.",
-    img: "/weather_app-browser-mockup.jpeg",
+    img: "/weather_app-browser-mockup.avif",
     public_url: "https://weather-app-rishisoyal.vercel.app/",
     github: "https://github.com/rishisoyal/Weather-App",
     techStack: ["Javascript", "Svelte"],
@@ -20,7 +20,7 @@ export const projects: Array<Project> = [
   {
     name: "My Portfolio Website",
     desc: "A personal portfolio website to showcase my projects, skills, and experience. Built with SvelteKit and Tailwind CSS, featuring dark mode and custom cursor.",
-    img: "/portfolio-browser-mockup.jpeg",
+    img: "/portfolio-browser-mockup.avif",
     public_url: "https://rishisoyal.pages.dev",
     github: "https://github.com/rishisoyal/portfolio-in-svelte",
     techStack: [
@@ -35,7 +35,7 @@ export const projects: Array<Project> = [
   {
     name: "Product Management System",
     desc: "A simple python TUI to manage and track customers and products",
-    img: "/weather_app.avif",
+    img: "/weather_app-browser-mockup.avif",
     github: "https://github.com/rishisoyal/Product-Management-System",
     techStack: ["Python", "Pandas Library"],
   },
