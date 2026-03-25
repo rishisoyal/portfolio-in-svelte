8631fb1ca4523ad4302f6c5abdc3a0f26c0f6b89
rishisoyal <rishisoyal510@gmail.com>
2026-03-25 10:20:07 +0530

    fix: toast flowing out in mobile screen

--- src/components/ui/Toast.svelte
+++ src/components/ui/Toast.svelte
@@ -11,34 +11,38 @@
 </script>
 
 {#if $toast.open}
-	<div
-		class="animation-fade-in-right fixed right-0 z-999 m-2 flex h-25 min-w-100 items-center justify-end gap-4 overflow-hidden rounded-xl border-2 p-4 backdrop-blur-xs"
-		style={`
+	<div class="animation-slide-in-right fixed right-0 z-999 w-full p-4 md:w-100">
+		<div
+			class="flex w-full items-center justify-end gap-4 overflow-hidden rounded-xl border-2 p-4 backdrop-blur-xs md:h-18"
+			style={`
             background-color: ${severityColor[$toast.severity].bgColor};
             border-color: ${severityColor[$toast.severity].borderColor};
 						`}
-		out:fade={{
-			duration: 400
-		}}
-	>
-		<div
-			class="toast-bar full absolute inset-0 h-0.5 rounded-2xl bg-[#5160b2] brightness-110"
-			style="--duration: {$toast.duration}ms"
-		></div>
-		<div class="flex h-full w-full items-center justify-between">
-			{$toast.message}
-			<button
-				onclick={hideToast}
-				class="flex h-full cursor-pointer items-start justify-start custom-cursor:cursor-none"
-				title="dismiss"
-			>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-					><path
-						fill="currentColor"
-						d="m17.705 7.705l-1.41-1.41L12 10.59L7.705 6.295l-1.41 1.41L10.59 12l-4.295 4.295l1.41 1.41L12 13.41l4.295 4.295l1.41-1.41L13.41 12z"
-					/></svg
-				>
-			</button>
+			out:fade={{
+				duration: 400
+			}}
+		>
+			<div
+				class="toast-bar absolute inset-0 h-0.5 rounded-2xl bg-[#5160b2] brightness-110"
+				style="--duration: {$toast.duration}ms"
+			></div>
+			<div class="flex h-full w-full items-center justify-between">
+				{$toast.message}
+				<div class="flex h-full items-start">
+					<button
+						onclick={hideToast}
+						class="cursor-pointer custom-cursor:cursor-none"
+						title="Dismiss"
+					>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+							><path
+								fill="currentColor"
+								d="m17.705 7.705l-1.41-1.41L12 10.59L7.705 6.295l-1.41 1.41L10.59 12l-4.295 4.295l1.41 1.41L12 13.41l4.295 4.295l1.41-1.41L13.41 12z"
+							/></svg
+						>
+					</button>
+				</div>
+			</div>
 		</div>
 	</div>
 {/if}
